6c5f0e924569c6e7d643afc6e166882222ddffd3
Tatlex <51352465+Tatlex@users.noreply.github.com>
2022-05-26 19:11:53 +0300

    Initial commit

---
+++ Portfolio/task/Scripts/script.js
@@ -0,0 +1,15 @@
+function togglePopUp1(){
+    document.getElementById("window-1").classList.toggle("active");
+}
+
+function togglePopUp2(){
+    document.getElementById("window-2").classList.toggle("active");
+}
+
+function togglePopUp3(){
+    document.getElementById("window-3").classList.toggle("active");
+}
+
+function togglePopUp4(){
+    document.getElementById("window-4").classList.toggle("active");
+}
